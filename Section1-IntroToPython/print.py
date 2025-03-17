b61f52567c6e289fae8a2c878e216f29398c6efb
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2025-03-16 22:57:25 -0700

    First commit introducing operations, the print function, variables, and strings

---
+++ Section1-IntroToPython/print.py
@@ -0,0 +1,55 @@
+# Using the print() function (built-in)
+x = 5+9
+print(x)
+# Output?
+print('************************')
+x = 7
+x = x + 1
+x = x - 3
+print(x)
+# Output?
+print('************************')
+x = 12 % 5
+print(x)
+# Output?
+print('************************')
+
+x = 12
+y = 9
+z = 3
+print(x+y-z)
+# Output?
+print('************************')
+
+my_str = 'helloyou'
+print(my_str)
+# Output?
+print('************************')
+
+my_str = 'helloyou'
+print(my_str.upper())
+# Output?
+print('************************')
+
+my_str = 'youareawesome'
+print(my_str[0:3], my_str[3:6], my_str[6:])
+# Output?
+print('************************')
+
+x = 2**3 * 3 / 2
+print(x)
+# Output?
+print('************************')
+
+# The formula for the area of a circle is pi*r^2 where r is the radius
+pi = 3.14
+r = 0.5
+area = pi*r**2
+print(area)
+
+
+
+
+
+
+
